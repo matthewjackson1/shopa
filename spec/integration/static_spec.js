cd2dade3414c89288fa9106ad4dd487931819ed9
Matthew Jackson <matthewj91@hotmail.com>
2018-06-09 17:35:40 +0100

    setup_4 complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,12 +8,10 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 //#2
-    it("should return status code 200", (done) => {
-
-//#3
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-
+it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+  request.get(base, (err, res, body) => {
+    expect(res.statusCode).toBe(200);
+    expect(body).toContain("Welcome to Bloccit");
 //#4
         done();
       });
--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use("/", (req, res, next) => {
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
-appConfig.init();
+appConfig.init(app, express);
 routeConfig.init(app);
 
 module.exports = app;
