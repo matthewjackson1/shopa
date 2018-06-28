92281174d35f56334845e8b7db12c5fd0f82e274
Matthew Jackson <matthewj91@hotmail.com>
2018-06-29 00:58:29 +0100

    amending routes

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,9 +1,5 @@
 module.exports = {
     index(req, res, next){
-        res.render("static/index", {title: "Welcome to Bloccit"});
-    },
-
-    test(req, res) {
-        res.send([{text: "Foo"}]);
+        res.render("static/index");
     }
   }
--- src/routes/static.js
+++ src/routes/static.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
-router.get("/test", staticController.test);
+
 
 
 module.exports = router;
